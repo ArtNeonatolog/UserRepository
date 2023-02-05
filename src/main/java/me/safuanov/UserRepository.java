7d639acd7303128ff86b175ef82790940b9ec4c0
ArtNeonatolog <marcus201@mail.ru>
2023-02-05 23:36:01 +0500

    Исправил.

--- src/main/java/me/safuanov/UserRepository.java
+++ src/main/java/me/safuanov/UserRepository.java
@@ -3,33 +3,29 @@ package me.safuanov;
 import java.util.*;
 
 public class UserRepository {
-    private final User user = new User(getUser().getLogin(), getUser().getPassword());
-
-    private final Optional<User> optionalUser = Optional.of(user);
-
+    private final User user;
     private final List<User> userList = new ArrayList<>();
+
+    public UserRepository(User user) {
+        this.user = user;
+    }
+
     public User getUser() {
 
-        return user;
+        return this.user;
     }
 
     public void addToList (User user) {
+
         userList.add(user);
     }
 
     public Optional<User> searchLogin (String login) {
-        if (optionalUser.stream().anyMatch(user -> user.getLogin().equals(login))){
-            return optionalUser;
-        }
-        return Optional.empty();
+        return this.userList.stream().filter(user -> user.getLogin().equals(login)).findAny();
     }
 
     public Optional<User> searchLoginAndPassword (String login, String password) {
-        if (optionalUser.stream().allMatch(user -> user.getLogin().equals(login)) &&
-                optionalUser.stream().allMatch(user -> user.getPassword().equals(password))){
-            return optionalUser;
-        }
-        return Optional.empty();
+        return this.userList.stream().filter(user -> user.getLogin().equals(login)).filter(user -> user.getPassword().equals(password)).findAny();
     }
 
     public Collection<User> getAllUsers () {
